c7966687cc03208666ec9139d15d963aaa0d7c5a
earnwithalee <earnwithalee@gmail.com>
2026-04-27 17:04:25 +0500

    refactor: consolidate payment configuration helpers

---
+++ server/utils/payment.js
@@ -0,0 +1,12 @@
+import { STXtoMicroSTX } from 'x402-stacks';
+import { SERVER_ADDRESS, NETWORK, FACILITATOR_URL } from '../config/index.js';
+
+export function createPaymentConfig(amountSTX, description) {
+  return {
+    amount: STXtoMicroSTX(amountSTX),
+    payTo: SERVER_ADDRESS,
+    network: NETWORK,
+    facilitatorUrl: FACILITATOR_URL,
+    description,
+  };
+}
